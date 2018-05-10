09050913f988bcc4287fe36f235c40424c223342
Brittanie Pham <brittaniepham@gmail.com>
2018-05-10 16:32:48 -0600

    method restructure

--- questions.rb
+++ questions.rb
@@ -1,22 +1,25 @@
-class Questions
+require_relative 'eight_ball_group'
 
+class Questions
   def initialize
+    puts "Welcome to the Magic Eight Ball"
+    welcome
     user_input
   end
 
   def welcome
-    puts "Welcome to the Magic Eight Ball"
     puts "Type a question to get an answer, or type QUIT to end the program"
   end
 
   def user_input
-    welcome
     @input = gets.strip
     if @input == 'QUIT'
+      puts "Thanks for playing!"
       exit
     else
+      question = Answers.new
     end
-
   end
 end
 
+welcome = Questions.new
