896e3ef5fb90345529db1e6f2371e6988b27097f
Brittanie Pham <brittaniepham@gmail.com>
2018-05-10 16:04:05 -0600

    welcome and user input method

--- questions.rb
+++ questions.rb
@@ -1,7 +1,22 @@
 class Questions
 
   def initialize
-    
+    user_input
   end
 
+  def welcome
+    puts "Welcome to the Magic Eight Ball"
+    puts "Type a question to get an answer, or type QUIT to end the program"
+  end
+
+  def user_input
+    welcome
+    @input = gets.strip
+    if @input == 'QUIT'
+      exit
+    else
+    end
+
+  end
 end
+
